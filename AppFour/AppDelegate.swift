4c8259bf3754c913ea15e2968102ba8e0698e3fe
Mykola Grymalyuk <mykolagrymalyuk@Mykolas-MacBook-Pro.local>
2018-07-13 20:22:45 -0600

    WarCardGame
    First full fledged iOS project, requires XCode 10 or AppDelegate.swift modification

--- AppFour/AppDelegate.swift
+++ AppFour/AppDelegate.swift
@@ -14,9 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -42,5 +43,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
-}
+
 
--- AppFour/ViewController.swift
+++ AppFour/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +34,51 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let feedbackGenerator = UIImpactFeedbackGenerator(style: .heavy)
+        feedbackGenerator.impactOccurred()
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        print("leftRandomNumber is: \(leftRandomNumber)")
+        
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        print("rightRandomNumber is: \(rightRandomNumber)")
+        //change image views
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        if leftRandomNumber == rightRandomNumber {
+            print("my dick is definatly bigger")
+        }
+        if leftRandomNumber > rightRandomNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        if leftRandomNumber < rightRandomNumber {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+    }
+    
+    @IBAction func resetScore(_ sender: Any) {
+        
+        //resets score label
+        leftScore = 0
+        leftScoreLabel.text = String(leftScore)
+        rightScore = 0
+        rightScoreLabel.text = String(rightScore)
+       
+        //allows user to recognize reset
+        let feedbackGenerator = UIImpactFeedbackGenerator(style: .heavy)
+        feedbackGenerator.impactOccurred()
+       
+        //resets the Cards image
+        leftImageView.image = UIImage(named: "back")
+        rightImageView.image = UIImage(named: "back")
+    }
 }
 
